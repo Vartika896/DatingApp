b9f4e7d586d27c0bba3e53bee9aebbb98f5950b5
swapnil dubey <swapnild2.kval@gmail.com>
2023-10-18 18:24:27 +0200

    Added Angular project- DatingApp

--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
@@ -17,6 +18,7 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseCors(builder=> builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
